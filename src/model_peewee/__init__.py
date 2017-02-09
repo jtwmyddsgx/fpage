a6f2b2faf8fa22af4320c7514e0e69ab94ea8b73
jtwmyddsgx <jtwmyddsgx@163.com>
2017-02-09 21:29:45 +0800

    将model_to_dict改写并添加到BaseModel中作为实例方法

--- src/model_peewee/__init__.py
+++ src/model_peewee/__init__.py
@@ -4,7 +4,7 @@ import math
 import config
 import peewee
 from playhouse.db_url import connect
-from playhouse.shortcuts import model_to_dict
+from playhouse.shortcuts import _clone_set
 
 db = connect(config.DATABASE_URI)
 
@@ -13,8 +13,76 @@ class BaseModel(peewee.Model):
     class Meta:
         database = db
 
-    def to_dict(self):
-        return model_to_dict(self)
+    def model_to_dict(self, recurse=True, backrefs=False, only=None,
+                      exclude=None, seen=None, extra_attrs=None,
+                      fields_from_query=None, max_depth=None):
+        model = self
+        max_depth = -1 if max_depth is None else max_depth
+        if max_depth == 0:
+            recurse = False
+
+        only = _clone_set(only)
+        extra_attrs = _clone_set(extra_attrs)
+
+        if fields_from_query is not None:
+            for item in fields_from_query._select:
+                if isinstance(item, Field):
+                    only.add(item)
+                elif isinstance(item, Node) and item._alias:
+                    extra_attrs.add(item._alias)
+
+        data = {}
+        exclude = _clone_set(exclude)
+        seen = _clone_set(seen)
+        exclude |= seen
+        model_class = type(model)
+
+        for field in model._meta.declared_fields:
+            if field in exclude or (only and (field not in only)):
+                continue
+
+            field_data = model._data.get(field.name)
+            if isinstance(field, peewee.ForeignKeyField) and recurse:
+                if field_data:
+                    seen.add(field)
+                    rel_obj = getattr(model, field.name)
+                    field_data = rel_obj.model_to_dict(recurse=recurse, backrefs=backrefs, only=only, exclude=exclude,
+                                                       seen=seen, max_depth=max_depth - 1)
+                else:
+                    field_data = {}
+
+            data[field.name] = field_data
+
+        if extra_attrs:
+            for attr_name in extra_attrs:
+                attr = getattr(model, attr_name)
+                if callable(attr):
+                    data[attr_name] = attr()
+                else:
+                    data[attr_name] = attr
+
+        if backrefs and recurse:
+            for related_name, foreign_key in model._meta.reverse_rel.items():
+                descriptor = getattr(model_class, related_name)
+                if descriptor in exclude or foreign_key in exclude:
+                    continue
+                if only and (descriptor not in only) and (foreign_key not in only):
+                    continue
+
+                accum = []
+                exclude.add(foreign_key)
+                related_query = getattr(
+                    model,
+                    related_name + '_prefetch',
+                    getattr(model, related_name))
+
+                for rel_obj in related_query:
+                    accum.append(rel_obj.model_to_dict(recurse=recurse, backrefs=backrefs, only=only, exclude=exclude,
+                                                       max_depth=max_depth - 1))
+
+                data[related_name] = accum
+
+        return data
 
     @classmethod
     def get_by_pk(cls, value):
